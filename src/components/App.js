4af4ea287518f576c34323705e3e040f80fae9e0
euan.may24@gmail.com <euan.may24@gmail.com>
2024-07-11 23:20:42 +0900

    [Feat] sort로 최신순 칼로리높은순 버튼 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import FoodList from './FoodList';
 import items from '../mock.json';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
-      <FoodList items={items} />
-    </div>
-  );
+    const [order, setOrder] = useState('createAt');
+    const sortedItems = [...items].sort((a, b) => b[order] - a[order]);  // 내림차순
+    // 스프레드 연산자로 original array 변경을 방지
+    const handleNewestClick = () => setOrder('createAt');
+    const handleCalorieClick = () => setOrder('calorie');
+
+    return (
+        <div>
+            <div>
+                <button onClick={handleNewestClick}>최신순</button>
+                <button onClick={handleCalorieClick}>칼로리순</button>
+            </div>
+            <FoodList items={sortedItems}/>
+        </div>
+    );
 }
 
 export default App;
